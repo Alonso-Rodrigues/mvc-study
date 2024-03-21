05431f652b1ea8ceeae8ddeb27b74e2bb3c4d8a8
Alonso <alonso.rbass@gmail.com>
2024-03-21 03:02:33 +0100

    update: "style menu"

--- app/views/top.php
+++ app/views/top.php
@@ -8,27 +8,26 @@
 </head>
 
 <body>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="<?= url ?>" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2 " width="40" height="32"></svg>
-            <span class="fs-4 p-4 border border-dark rounded-circle bg-dark text-light">M.V.C</span>
+    <header class="d-flex flex-wrap justify-content-around py-4 mb-4 border-bottom">
+        <a href="<?= url ?>">
+            <img class="rounded-circle" src="<?= url ?>/public/img/logo.jpg" alt="logo" style="width: 130px; height: 90px;">
         </a>
 
-        <ul class="nav nav-pills">
-            <li class="nav-item" style="margin-right: 50px;">
+        <ul class="nav nav-pills d-flex align-items-center">
+            <li class="nav-item">
                 <a href="<?= url ?>" class="nav-link active" aria-current="page">Home</a>
             </li>
             <li class="nav-item">
-                <a href="<?= url ?>/pages/about" class="nav-link" style="margin-right: 25px;">About</a>
+                <a href="<?= url ?>/pages/about" class="nav-link">About</a>
             </li>
             <?php if (isset($_SESSION['user_id'])) { ?>
-                <p class="nav-link" style="margin-right: 25px;">
+                <li class="nav-link">
                     Welcome, <?= $_SESSION['user_name']; ?>
-                    <a class="btn btn-sm btn-danger" style="margin-left: 50px" href="<?= url ?>/users/logOut">Log Out</a>
-                </p>
+                </li>
+                <a class="btn btn-sm btn-danger" href="<?= url ?>/users/logOut">Log Out</a>
             <?php } else { ?>
-                <li class="nav-item"><a href="<?= url ?>/users/register" class="nav-link" style="margin-right: 50px;">Register</a></li>
-                <li class="nav-item"><a href="<?= url ?>/users/login" class="nav-link" style="margin-right: 50px;">Login</a></li>
+                <li class="nav-item"><a href="<?= url ?>/users/register" class="nav-link">Register</a></li>
+                <li class="nav-item"><a href="<?= url ?>/users/login" class="nav-link">Login</a></li>
             <?php } ?>
         </ul>
 
